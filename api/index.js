bfd26d1f5355d504a53e48d5c2ce94b989364f02
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-02-02 21:18:42 +0100

    build: add swagger documentation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,10 @@ dotenv.config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const routerTravel = require('./routers/travel.router');
 
 const server = express();
 
@@ -18,6 +22,26 @@ server.use(express.static('assets'));
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+
+/**
+ * Import and define swagger doc
+ */
+/** Swagger Initialization - START */
+const swaggerOption = {
+    swaggerDefinition: (swaggerJsdoc.Options = {
+        info: {
+            title: "API TourTracker",
+            description: "API documentation",
+            servers: [`http://localhost:${process.env.PORT_SERVER}/`],
+        },
+    }),
+    apis: ["index.js", "./routers/*.js"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOption);
+server.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// redirection
+server.use('/travel', routerTravel);
 
 server.listen(process.env.PORT_SERVER, () => {
     console.log(`Server is listening port ${process.env.PORT_SERVER}`);
