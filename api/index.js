6be5cffe19c30b05b69e571f80459e511b3ecdf9
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-02-02 20:57:04 +0100

    feat: add config to run the server

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const express = require('express');
+
+const server = express();
+
+// utilisation des variables d'environnement
+dotenv.config();
+
+// utilisation des requetes CORS
+server.use(cors());
+
+// lancement du serveur
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static('assets'))
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.listen(process.env.PORT_SERVER, () => {
+    console.log(`Server is listening port ${process.env.PORT_SERVER}`);
+});
