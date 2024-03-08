8e8b5b45b9d5278fb044f014af12e3786ab33293
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-03-08 22:06:06 +0100

    chore: minor changes

--- api/controllers/travel.controller.js
+++ api/controllers/travel.controller.js
@@ -7,9 +7,6 @@ const getAllTravel = (req, res) => {
         if (err) {
             return res.status(500).send({ error: err.message });
         }
-        if (data.length === 0) {
-            return res.status(404).send({ error: error.notFound });
-        }
         return res.status(200).send({ data: data });
     });
 }
